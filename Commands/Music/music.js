c07e97e7a21e2440e61f1612a0246c9ef7d16ecb
Antonis Rusman <antonisxgr05@gail.com>
2023-09-05 23:55:19 +0300

    Update

---
+++ Commands/Music/music.js
@@ -0,0 +1,116 @@
+const { VolumeInterface } = require("discord.js")
+const { EmbedBuilder, SlashCommandBuilder, PermissioFlagsBits, VoiceChannel, GuildEmoji } = require("discord.js");
+const client = require("../../index");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("music")
+    .setDescription("Complete music system.")
+    .addSubcommand(subcommand =>
+        subcommand.setName("play")
+        .setDescription("Play a song.")
+        .addStringOption(option =>
+            option.setName("query")
+                .setDescription("Provide the name or the url for the song.")
+                .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+        subcommand.setName("volume")
+        .setDescription("Adjust the song volume.")
+        .addIntegerOption(option =>
+            option.setName("percent")
+                .setDescription("10 = 10%")
+                .setMinValue(1)
+                .setMaxValue(100)
+                .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+        subcommand.setName("options")
+        .setDescription("Select an option.")
+        .addStringOption(option =>
+            option.setName("options")
+                .setDescription("Select an option.")
+                .setRequired(true)
+                .addChoices(
+                    {name: "queue", value: "queue"},
+                    {name: "skip", value: "skip"},
+                    {name: "pause", value: "pause"},
+                    {name: "resume", value: "resume"},
+                    {name: "stop", value: "stop"},
+                )
+        )
+    ),
+    async execute(interaction) {
+        const {options, member, guild, channel} = interaction;
+
+        const subcommand = options.getSubcommand();
+        const query = options.getString("query");
+        const volume = options.getInteger("percent");
+        const option = options.getString("options");
+        const voiceChannel = member.voice.channel;
+
+        const embed = new EmbedBuilder();
+
+        if (!voiceChannel) {
+            embed.setColor("Red").setDescription("You must be in a voice channel to execute this command.");
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
+        if (!member.voice.channelId == guild.members.me.voice.channelId) {
+            embed.setColor("Red").setDescription(`You can't use this music player as it is already active in <#${guild.members.me.voice.channelId}>`);
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
+        try {
+            switch (subcommand) {
+                case "play":
+                    client.distube.play(voiceChannel, query, { textChannel: channel, member: member});
+                    return interaction.reply({ content: "🎶 Request received." });
+
+                case "volume":
+                    client.distube.setVolume(voiceChannel, volume);
+                    embed.setColor("fffffe").setDescription(`🔉 Volume has been set to ${volume}%.`);
+                    return interaction.reply({ embeds: [embed], ephemeral: true});
+                case "options":
+                    const queue = await client.distube.getQueue(voiceChannel);
+
+                    if(!queue) {
+                        embed.setColor("Red").setDescription("There is no active queue.");
+                        return interaction.reply({ embeds: [embed], ephemeral: true});
+                    }
+
+                    switch(option) {
+                        case "skip":
+                            await queue.skip(voiceChannel);
+                            embed.setColor("Blue").setDescription("⏩ The song has been skipped.");
+                            return interaction.reply({ embeds: [embed], ephemeral: true});
+                        case "stop":
+                            await queue.stop(voiceChannel);
+                            embed.setColor("Red").setDescription("⏹ The queue has been stopped.");
+                            return interaction.reply({ embeds: [embed], ephemeral: true});
+                        case "pause":
+                            await queue.pause(voiceChannel);
+                            embed.setColor("Orange").setDescription("⏸ The song has been paused.");
+                            return interaction.reply({ embeds: [embed], ephemeral: true});
+                        case "resume":
+                            await queue.resume(voiceChannel);
+                            embed.setColor("Green").setDescription("⏯ The song has been resumed.");
+                            return interaction.reply({ embeds: [embed], ephemeral: true});
+                        case "queue":
+                            embed.setColor("fffffe").setDescription(`${queue.songs.map(
+                                (song, id) => `\n**${id + 1}.** ${song.name} -\`${song.formattedDuration}\``
+                            )}`);
+                            return interaction.reply({ embeds: [embed], ephemeral: true});
+                    }
+            }
+        } catch(err) {
+            console.log(err);
+
+            embed.setColor("Red").setDescription("⛔ | Something went wrong.");
+
+            return interaction.reply({ embeds: [embed], ephemeral: true});
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const { Client, GatewayIntentBits, Partials, Collection, EmbedBuilder } = require("discord.js");
 
-const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { Guilds, GuildMembers, GuildMessages, GuildVoiceStates } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember, Channel} = Partials;
 
 const { loadEvents } = require("./Handlers/eventHandler");
 const { loadCommands } = require("./Handlers/commandHandler");
 
 const client = new Client({
-    intents: [Guilds, GuildMembers, GuildMessages],
+    intents: [Guilds, GuildMembers, GuildMessages, GuildVoiceStates],
     partials: [User, Message, GuildMember, ThreadMember, Channel],
+});
+
+module.exports = client
+
+const { DisTube } = require('distube');
+const { SpotifyPlugin } = require("@distube/spotify");
+
+client.distube = new DisTube(client, {
+    emitNewSongOnly: true,
+    leaveOnFinish: true,
+    emitAddSongWhenCreatingQueue: false,
+    plugins: [new SpotifyPlugin()]
 });
 
 client.commands = new Collection();
