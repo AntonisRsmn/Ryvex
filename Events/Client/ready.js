a9004d2c72860e7619e7354b2cf253b32adaeb82
Antonis Rusman <antonisxgr05@gail.com>
2023-09-10 18:09:14 +0300

    Update

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,9 +1,15 @@
-const  { Client, ActivityType } = require("discord.js");
+const { Client, ActivityType } = require("discord.js");
+const mongoose = require("mongoose");
+const config = require("../../config.json");
 
 module.exports = {
     name: "ready",
     once: true,
-    execute(client) {
+    async execute(client) {
+        if (mongoose.connect) {
+            console.log("MongoDB Connected.")
+        }
+
         console.log(`${client.user.username} is now online.`);
         client.user.setPresence({
             activities: [{ name : `@${client.user.username}`, type: ActivityType.Listening }]
