136cef8d9cadac8d793bf343e70607add0be35eb
Antonis Rusman <antonisxgr05@gail.com>
2023-09-11 17:30:46 +0300

    Update

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -12,11 +12,17 @@ module.exports = {
 
         console.log(`${client.user.username} is now online.`);
 
+        const act = [
+            { name: `@${client.user.username}`, type: ActivityType.Listening },
+            { name: `${client.guilds.cache.size} Servers`, type: ActivityType.Watching },
+        ];
+
         setInterval(() => {
+            var random = act[Math.floor(Math.random() * act.length)];
             client.user.setPresence({
-                activities: [{ name : `@${client.user.username} | Servers: ${client.guilds.cache.size}`, type: ActivityType.Listening }]
-              })
-        }, 1000 * 10);
+                activities: [random]
+            })
+        }, 1000);
         
     },
 };
