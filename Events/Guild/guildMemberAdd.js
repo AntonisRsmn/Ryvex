85dc1c3a8a4ff56602201169e4769b6059f74234
Antonis Rusman <antonisxgr05@gail.com>
2023-09-12 16:35:07 +0300

    Update

---
+++ Events/Guild/guildMemberAdd.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require("@discordjs/builders");
+const { GuildMember, Embed } = require("discord.js");
+
+module.exports = {
+    name: "guildMemberAdd",
+    execute(member) {
+        const { user, guild } = member;
+
+        const welcomeChannel = member.guild.channels.cache.find(channel => channel.id === "1012717996470452335");
+
+        if(welcomeChannel) {
+            const role = member.guild.roles.cache.find(role => role.id === "1012717995451236445");
+            member.roles.add(role);
+
+            const welcomeEmbed = new EmbedBuilder()
+                .setTitle("***New member!***")
+                .setDescription(`Welcome <@${member.id}> to ***Zepp™ Support*** Hope you have great time here`)
+                .setColor(0xfffffe)
+                .setTimestamp();
+
+            welcomeChannel.send({ embeds: [welcomeEmbed] });
+        }
+    }
+}
