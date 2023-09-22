e9580fa5ad84acb00d4b5191f3d1a3f7596fcb59
Antonis Rusman <antonisxgr05@gail.com>
2023-09-22 22:27:28 +0300

    Update

--- Events/interactions/interactionCreate.js
+++ Events/interactions/interactionCreate.js
@@ -1,9 +1,15 @@
-const {CommandInteraction, InteractionResponse } = require("discord.js");
+const {CommandInteraction, InteractionResponse, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     name: "interactionCreate",
 
     execute(interaction, client) {
+        const embed = new EmbedBuilder()
+        .setTitle("Zepp™")
+        .setDescription("Commands can only be used inside servers.")
+        .setColor("fffffe")
+        .setTimestamp()
+
         if (!interaction.isChatInputCommand()) return;
 
         const command = client.commands.get(interaction.commandName);
@@ -11,6 +17,9 @@ module.exports = {
         if (!command) {
             interaction.reply({content: "outdated command"});
         }
+
+        if (!interaction.guild)
+            return interaction.reply({ embeds: [embed], ephemeral: true })
 
         command.execute(interaction, client);
     },
