8516636f0328834701e6ae38f5926eba77b53a2b
Antonis Rusman <antonisxgr05@gail.com>
2023-09-24 04:56:00 +0300

    Update

---
+++ Commands/Info/userinfo.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("userinfo")
+      .setDescription("Get info about a member.")
+      .addUserOption(option =>
+        option.setName("user")
+        .setDescription("User to get info.")
+        .setRequired(false)
+        ),
+
+    async execute(interaction) {
+      const { options } = interaction;
+      const user = options.getUser("user") || interaction.user;
+      const member = await interaction.guild.members.cache.get(user.id);
+      const icon = member.displayAvatarURL();
+      const tag = user.tag;
+      
+      const embed = new EmbedBuilder()
+        .setColor(0xFFFFFE)
+        .setAuthor({ name: tag, iconURL: icon })
+        .addFields(
+          { name: "Name: ", value: `${user}`, inline: false },
+          { name: "Roles: ", value: `${member.roles.cache.map(r => r).join(` ` )}`, inline: false },
+          { name: "Joined Server: ", value: `<t:${parseInt(member.joinedAt / 1000)}:R>`, inline: true },
+          { name: "Joined Discord: ", value: `<t:${parseInt(member.user.createdAt / 1000)}:R>`, inline: true },
+        )
+        .setFooter({ text: `User ID: ${user.id}`})
+        .setTimestamp()
+        
+      await interaction.reply({ embeds: [embed] });
+  }    
+}
