37675b2a6fda89d837a65b24eb0f3b090f553ae5
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2023-09-30 03:27:24 +0300

    Update

--- Commands/Music/play.js
+++ Commands/Music/play.js
@@ -14,7 +14,7 @@ module.exports = {
     async execute(interaction) {
         const {options, member, guild, channel} = interaction;
 
-        const query = options.getString("test");
+        const query = options.getString("query");
         const voiceChannel = member.voice.channel;
 
         const embed = new EmbedBuilder();
