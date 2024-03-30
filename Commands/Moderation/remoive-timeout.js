d6264ef3f8f83ed19f05c2b9417ffd1bea34a34e
Antonis Rusman <antonisxgr05@gail.com>
2024-03-30 15:10:02 +0200

    Update

---
+++ Commands/Moderation/remoive-timeout.js
@@ -0,0 +1,41 @@
+const { Client, SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("remove-timeout")
+        .setDescription("Remove timeout from a member of the guild.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+        .addUserOption(option => 
+            option.setName("target")
+                .setDescription("User to remove the timeout.")
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const { guild, options } = interaction;
+
+        const user = options.getUser("target");
+        const member = guild.members.cache.get(user.id);
+
+        const errEmbed = new EmbedBuilder()
+            .setDescription("Something went wrong")
+            .setColor(0xfffffe)
+
+        const embed = new EmbedBuilder()
+            .setTitle("**Timeout**")
+            .setDescription(`timeout removed succesfully from ${user}.`)
+            .setColor(0xfffffe)
+            .setTimestamp();
+
+        if (member.roles.highest.position >= interaction.member.roles.highest.position)
+            return interaction.reply({ embeds: [errEmbed], ephemeral: true});
+
+        try {
+            await member.timeout(null);
+
+            return interaction.reply({ embeds: [embed], ephemeral: true});
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
---
+++ Commands/Moderation/timeout.js
@@ -0,0 +1,61 @@
+const { Client, SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+const ms = require("ms");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("timeout")
+        .setDescription("Timeout a member from the guild.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+        .addUserOption(option => 
+            option.setName("target")
+                .setDescription("User to be timeout.")
+                .setRequired(true)
+        )
+        .addStringOption(option => 
+            option.setName("time")
+                .setDescription("How long should the timeout last (Up to 27 days).")
+                .setRequired(true)
+        )
+        .addStringOption(option => 
+            option.setName("reason")
+                .setDescription("Reason for the timeout.")
+        ),
+
+    async execute(interaction) {
+        const { guild, options } = interaction;
+
+        const user = options.getUser("target");
+        const member = guild.members.cache.get(user.id);
+        const time = options.getString("time");
+        const convertedTime = ms(time);
+        const reason = options.getString("reason") || 'No reason provided';
+
+        const errEmbed = new EmbedBuilder()
+            .setDescription("Something went wrong")
+            .setColor(0xfffffe)
+
+        const embed = new EmbedBuilder()
+            .setTitle("**Timeout**")
+            .setDescription(`Succesfully timed out ${user}.`)
+            .addFields(
+                { name: "Reason", value: `${reason}`, inline: true },
+                { name: "Duration", value: `${time}`, inline: true }
+            )
+            .setColor(0xfffffe)
+            .setTimestamp();
+
+        if (member.roles.highest.position >= interaction.member.roles.highest.position)
+            return interaction.reply({ embeds: [errEmbed], ephemeral: true});
+
+        if (!convertedTime)
+            return interaction.reply({ embeds: [errEmbed], ephemeral: true});
+
+        try {
+            await member.timeout(convertedTime, reason);
+
+            return interaction.reply({ embeds: [embed], ephemeral: true});
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
