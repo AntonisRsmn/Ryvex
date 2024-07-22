1747a03223ad7c2ea6830d0eb5668dfe370a7b09
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2024-07-22 22:57:53 +0300

    Update 22/07/2024 22:58 V1.8.7

--- Commands/Info/website.js
+++ Commands/Info/website.js
@@ -3,15 +3,15 @@ const { SlashCommandBuilder, EmbedBuilder, Client } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("website")
-    .setDescription("Zepp Website."),
+    .setDescription("Ryvex Website."),
 
     async execute(interaction, client) {
 
         const embed = new EmbedBuilder()
-        .setTitle(`***Zepp Website***`)
+        .setTitle(`***Ryvex Website***`)
         .setColor("#fffffe")
         .setTimestamp()
-        .setDescription("Check out our [Website](https://zepp.gr)")
+        .setDescription("Check out our [Website](https://ryvex.xyz)")
         .setFooter({
             text: `By ${interaction.user.username}`,
             iconURL: interaction.user.displayAvatarURL(),
